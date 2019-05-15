cf48498fd4e673b608956424bcc52dba86f659d0
Meov <zc9286@live.cn>
2019-05-15 20:44:12 +0800

    first_github_prject

---
+++ cmake_slambook/libfile/build/CMakeFiles/libHelloSLAM.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/libHelloSLAM.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake_slambook/libfile/libHelloSLAM.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+// this is a lib file for test
+using namespace std;
+void printHello()
+{
+	cout<<"hello slam from lib file" << endl;
+
+
+}
+
+
